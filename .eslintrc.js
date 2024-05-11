72a416189f1d2c4fd6d27e746b237cbee7f59417
Le Tien <tienle21032001@gmail.com>
2024-05-11 22:20:28 +0700

    config eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,16 @@
-/* eslint-disable prettier/prettier */
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
 };
